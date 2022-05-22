9cca8d32965a09f31c3f287d62a653480c37fadc
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-22 14:40:13 +0530

    Emit added and working in socket

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -10,7 +10,7 @@ let socket;
 function Chat() {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-  const END_POINT=`http://localhost:5000/`
+  const END_POINT=`http://localhost:5000/`;
   useEffect(() => {
    const {name,room}=queryString.parse(location.search);
 
@@ -19,7 +19,19 @@ function Chat() {
 
  setName(name);
  setRoom(room);
-  }, []);
+ console.log(name,room);
+
+socket.emit('join',{name,room},(message)=>{
+  console.log(message);
+
+});
+
+return ()=>{
+  socket.emit("disconnect");
+  socket.off();
+}
+
+  }, [END_POINT,location.search]);
 
   return <div className="chat">Chat</div>;
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 const cors = require("cors");
 const http = require("http");
 const router = require("./router");
@@ -8,21 +8,23 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server,{
+const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
-
-
 app.use(express.json());
-app.use(cors({origin:true}));
+app.use(cors({ origin: true }));
 app.use(router);
 
 io.on("connect", (socket) => {
   console.log("User connected now");
+
+  socket.on('join', ({name,room}) => {
+    console.log(name,room);
+  });
 
   socket.on("disconnect", () => {
     console.log("User disconnected");

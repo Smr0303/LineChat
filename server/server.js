154d232bfd1c9c9d9f64dfe2b34d15d731ca1e9f
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-22 23:11:22 +0530

    Message feature and broadcasting added

--- server/server.js
+++ server/server.js
@@ -15,16 +15,39 @@ const io = new Server(server, {
   },
 });
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+
 app.use(express.json());
 app.use(cors({ origin: true }));
 app.use(router);
 
 io.on("connect", (socket) => {
-  console.log("User connected now");
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-  socket.on('join', ({name,room}) => {
-  
+    if (error) {
+      return callback(error);
+    }
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name},welcome to the room ${user.room}`,
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    socket.join(user.room);
+    callback();
   });
+
+  socket.on('sendMessage',(message,callback)=>{
+ const user=getUser(socket.id);
+ io.to(user.room).emit('message',{user:user.name,message:message});
+
+ callback();
+  })
 
   socket.on("disconnect", () => {
     console.log("User disconnected");

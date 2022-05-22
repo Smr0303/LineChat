c9a2d4859da385d8aa47b132bec37a9dc9e35503
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-22 13:46:38 +0530

    Socket io working

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const socketio = require("socket.io");
+const {Server} = require("socket.io");
 const cors = require("cors");
 const http = require("http");
 const router = require("./router");
@@ -8,11 +8,27 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = new Server(server,{
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:true}));
 app.use(router);
+
+io.on("connect", (socket) => {
+  console.log("User connected now");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
 server.listen(port, () => {
-  console.log("started",port);
+  console.log("started", port);
 });

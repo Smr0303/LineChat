3f8c38533b69f5383629c8eaba44dea1f2aa83d9
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-22 13:47:10 +0530

    Socketio working

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,9 +1,27 @@
-import React from 'react'
-import './Chat.css';
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import {io} from "socket.io-client";
+import "./Chat.css";
+
+
+let socket;
+
+
 function Chat() {
-  return (
-    <div>Chat</div>
-  )
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const END_POINT=`http://localhost:5000/`
+  useEffect(() => {
+   const {name,room}=queryString.parse(location.search);
+
+   socket=io(END_POINT);
+   console.log(socket);
+
+ setName(name);
+ setRoom(room);
+  }, []);
+
+  return <div className="chat">Chat</div>;
 }
 
-export default Chat
+export default Chat;

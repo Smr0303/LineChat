cae5f68339eeb6040b739884f6275f6d99b788e1
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-28 14:22:25 +0530

    Message component added

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import ScrollToBottom from 'react-scroll-to-bottom'
-import './Messages.css'
-function Messages({messages,name}) {
-  return (
-    <div><ScrollToBottom>
-  {messages.map((ele,i)=>{
- <div key={i}><Message message={message} name={name}/></div>
-
-  })}
-
-
-      </ScrollToBottom></div>
-  )
+import React from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+import "./Messages.css";
+import Message from "../Message/Message";
+function Messages({ messages, name }) {
+  return ( 
+    <div>
+      <ScrollToBottom>
+        {messages.map((ele, i) => {
+          <div key={i}>
+            <Message message={message} name={name} />
+          </div>;
+        })}
+      </ScrollToBottom>
+    </div>
+  );
 }
 
-export default Messages
+export default Messages;
